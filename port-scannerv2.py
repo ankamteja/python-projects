f0fde8d398bdd300f50cd3759544f91494bfb7ea
charan <ankamcharantejaa@gmail.com>
2026-05-19 18:56:15 +0530

    day 5: port scanner using multi-threading and banner print

---
+++ port-scannerv2.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+from queue import Queue
+
+print_lock = threading.Lock
+
+target = input("Enter IP or Domain Name: ")
+
+q = Queue()
+
+def scan_port(port):
+    s = socket.socket()
+    s.settimeout(1)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        try:
+            banner = s.recv(1024).decode().strip()
+        except:
+            banner="no banner"
+        print(f"Port {port}: OPEN | {banner}")
+    s.close()
+
+def threader():
+    while True:
+        port=q.get()
+        scan_port(port)
+        q.task_done()
+
+for x in range(100):
+    t = threading.Thread(target=threader)
+    t.daemon=True
+    t.start()
+
+for port in range(1,1024):
+    q.put(port)
+
+q.join()
+print("scan complete")
